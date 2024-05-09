a92cae39f0e013a82427a2939e699be96a24292e
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-05-09 11:41:42 +0300

    Add Permutations without Repetitions

---
+++ Permutations without Repetitions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine().Split();
+            GeneratePermutate(0, elements);
+        }
+        public static void GeneratePermutate(int index, string[] permutate)
+        {
+            if (index >= permutate.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutate));
+            }
+            else
+            {
+                GeneratePermutate(index + 1, permutate);
+
+                for (int i = index + 1; i < permutate.Length; i++)
+                {
+                    Swap(index, i, permutate);
+                    GeneratePermutate(index + 1, permutate);
+                    Swap(i, index, permutate);
+                }
+            }
+            static void Swap(int i, int j, string[] permutate)
+            {
+                string temp = permutate[i];
+                permutate[i] = permutate[j];
+                permutate[j] = temp;
+            }
+        }
+    }
+}
