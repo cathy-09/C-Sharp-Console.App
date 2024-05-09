2568fcbaad4356a89ba5132210ea79e277d53ce8
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-05-09 12:11:17 +0300

    Variations with Repetitions

---
+++ Variations with Repetitions/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace task4
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int k = int.Parse(Console.ReadLine());
+            int[] vectorVariations = new int[k];
+            while (true)
+            {
+                Print(vectorVariations, input);
+                int index = k - 1;
+                while (index >= 0 && vectorVariations[index] == input.Length - 1)
+                {
+                    index--;
+                }
+                if (index < 0)
+                {
+                    break;
+                }
+                vectorVariations[index]++;
+                for (var i = index + 1; i < vectorVariations.Length; i++)
+                {
+                    vectorVariations[i] = 0;
+                }
+            }
+        }
+
+        private static void Print(int[] vectorVariations, string[] input)
+        {
+            foreach (var index in vectorVariations)
+            {
+                Console.Write(input[index] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
