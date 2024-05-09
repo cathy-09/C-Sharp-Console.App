930578bba2826efd8cd84248fcde55da82357c96
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-05-09 12:16:43 +0300

    Add files Binom

---
+++ Binom/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task7
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine(Binom(n, k));
+        }
+        private static ulong Binom(int n, int k)
+        {
+            if (k > n)
+            {
+                return 0;
+            }
+
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+            return Binom(n - 1, k - 1) + Binom(n - 1, k);
+        }
+    }
+}
