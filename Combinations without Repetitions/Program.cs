55983930a501e65b3c6e5008b877d948420c0da9
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-05-09 12:16:10 +0300

    Add files Combinations without Repetitions

---
+++ Combinations without Repetitions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task6
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int k = int.Parse(Console.ReadLine());
+            Gen(new string[k], input, 0, 0);
+        }
+        private static void Gen(string[] vectorCombinations,
+            string[] input,
+            int originalIndex,
+            int customI)
+        {
+            if (originalIndex >= vectorCombinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", vectorCombinations));
+            }
+            else
+            {
+                for (var i = customI; i < input.Length; i++)
+                {
+                    vectorCombinations[originalIndex] = input[i];
+                    Gen(vectorCombinations, input, originalIndex + 1, i);
+                }
+            }
+        }
+    }
+}
