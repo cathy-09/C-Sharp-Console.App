9ca453b144a34ebe9a9a65d6dc0daeff1ecb4958
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-05-09 11:53:33 +0300

    Add files Variations without Repetitions

---
+++ Variations without Repetitions/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            int k = int.Parse(Console.ReadLine());
+            Gen(input, new string[k], new bool[input.Length], k, 0);
+        }
+
+        private static void Gen(string[] input,
+            string[] vectorVariations,
+            bool[] used,
+            int k,
+            int index)
+        {
+            if (index >= k)
+            {
+                Console.WriteLine(string.Join(" ", vectorVariations));
+            }
+            else
+            {
+                for (var i = 0; i < input.Length; i++)
+                {
+                    if (used[i])
+                    {
+                        continue;
+                    }
+                    used[i] = true;
+                    vectorVariations[index] = input[i];
+                    Gen(input, vectorVariations, used, k, index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
