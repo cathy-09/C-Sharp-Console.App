1f2a3172fe1f159e4dd4f40e6dfdf8245934672d
gerganakaloferova <158329994+cathy-09@users.noreply.github.com>
2024-05-30 12:05:36 +0300

    Dictionary task1

---
+++ Dictionaries/Брой на еднакви стойности в масив/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            Dictionary<double, int> result = new Dictionary<double, int>();
+            foreach (var item in nums)
+            {
+                if (result.ContainsKey(item))
+                {
+                    result[item]++;
+                }
+                else
+                {
+                    result[item] = 1;
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times.");
+            }
+        }
+    }
+}
